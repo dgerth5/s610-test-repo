ca5e1d1d94ab8c841bc516b3caac93dbc4c800bb
dgerth5 <davidgerth5@gmail.com>
2024-10-30 13:26:26 -0400

    add microbenchmark

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,14 +1,24 @@
 source("llr_functions.R")
-# Try out functions to see if it works on some data:
-library(reshape2) # package that contains "french_fries"
+
+library(reshape2)
+library(microbenchmark)  
 data(french_fries)
+
+# Prepare data
 french_fries = french_fries[complete.cases(french_fries),]
 z = seq(0, 15, length.out = 100)
-fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 2)
-plot(z, fits)
 
-# Loop through different omega values
-for (i in 1:10){
-  fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = i)
-  plot(z, fits, main = paste("llr with ω =", i),  xlab= expression(z), ylab = expression(hat(f)(z)))
+# Define omega values to test
+omega_values = 1:10
+
+library(microbenchmark)
+
+for (omega in omega_values) {
+  timing = microbenchmark(
+    llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = omega),
+    times = 10  
+  )
+
+  cat("Omega =", omega, "\n")
+  print(timing)
 }
